deb954d1da1cc98cd0230758ed16de4a77a0edbc
nicolaschiong <kulastv@gmail.com>
2018-02-03 23:06:46 +0800

    Initial commit

---
+++ learnphp.php
@@ -0,0 +1,25 @@
+<?php
+
+class Animal{
+    protected $name;
+    protected $favorite_food;
+    protected $sound;
+    protected $id;
+
+    public static $number_of_animals = 0;
+
+    const PI = "3.14159";
+
+    function getName(){
+        
+    }
+
+}
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 03/02/2018
+ * Time: 11:00 PM
+ */
+
+
